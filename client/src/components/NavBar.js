b3a0906457941709abe4c8732e17503d992563a9
Emma Andrews <eandrews258@gmail.com>
2020-02-01 12:23:23 -0500

    Basics pages

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 
 const NavBar = (props) => {
   return (
     <>
-      <p>Navbar here</p>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Pomodoro</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/settings">Settings</Nav.Link>
+            <Nav.Link href="/account">Account</Nav.Link>
+            <Nav.Link href="/">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   );
 };
